8e72cee9b89c450e65e5e1d144806429f8fd6a29
kevinkim910408 <junho2413@gmail.com>
2023-03-21 16:15:00 -0400

    artists, songs update as request

--- w222/A4/Assignment4/src/js/artists.js
+++ w222/A4/Assignment4/src/js/artists.js
@@ -6,49 +6,29 @@
 
 window.artists = [
   /* TODO */
+  
   {
-    name: "Jay Park",
+    name: "Michael Jackson",
     id: "artist-01",
-    link1: "https://www.instagram.com/moresojuplease/",
-    linkName1: "Instagram",
-    link2: "https://www.youtube.com/jaybumaom0425",
-    linkName2: "YouTube"
+    link1: "https://en.wikipedia.org/wiki/Michael_Jackson",
+    linkName1: "Wikipedia",
+    link2: "https://www.michaeljackson.com",
+    linkName2: "Official site"
   },
   {
-      name: "IU",
+      name: "Stevie Wonder",
       id: "artist-02",
-      link1: "https://www.instagram.com/dlwlrma/",
-      linkName1: "Inatagram",
-      link2: "https://www.youtube.com/dlwlrma",
-      linkName2: "YouTube"
+      link1: "https://en.wikipedia.org/wiki/Stevie_Wonder",
+      linkName1: "Wikipedia",
+      link2: "http://www.steviewonder.net",
+      linkName2: "Official site"
   }, 
   {
-      name: "Ailee",
+      name: "Smokey Robinson",
       id: "artist-03",
-      link1: "https://www.instagram.com/aileeonline/",
-      linkName1: "Instagram",
-      link2: "https://www.youtube.com/aileemusic",
-      linkName2: "Youtube"
+      link1: "https://en.wikipedia.org/wiki/Smokey_Robinson",
+      linkName1: "Wikipedia",
+      link2: "https://smokeyrobinson.com",
+      linkName2: "Official site"
   }
 ];
-
-// var ArtistData = [
-//   {
-//     name:"Jay Park",
-//     id: "artist-01",
-//     link1: "https://www.instagram.com/moresojuplease/",
-//     link2: "Instagram"
-//   },
-//   {
-//       name:"IU",
-//       id: "artist-02",
-//       link1: "https://www.instagram.com/dlwlrma/",
-//       link2: "Instagram"
-//   }, 
-//   {
-//       name:"Ailee",
-//       id: "artist-03",
-//       link1: "https://www.instagram.com/aileeonline/",
-//       link2: "Instrgram"
-//   }
-// ];
--- w222/A4/Assignment4/src/js/songs.js
+++ w222/A4/Assignment4/src/js/songs.js
@@ -9,168 +9,168 @@ window.songs = [
   {
     songID: "s1",
     artistID: "artist-01",
-    title: "All I wanna do",
-    year: 2016,
+    title: "Billie Jean",
+    year: 1982,
     duration: 215,
     flagged: true
   },
   {
     songID: "s2",
     artistID: "artist-01",
-    title: "GANADARA",
-    year: 2022,
+    title: "Beat It",
+    year: 1982,
     duration: 204,
     flagged: true
   }, 
   {
     songID: "s3",
     artistID: "artist-01",
-    title: "Yesterday",
-    year: 2023,
+    title: "Thriller",
+    year: 1982,
     duration: 194,
     flagged: true
   },
   {
     songID: "s4",
     artistID: "artist-01",
-    title: "JOAH",
-    year: 2013,
+    title: "Heal the World",
+    year: 1991,
     duration: 230,
     flagged: true
   },
   {
     songID: "s5",
     artistID: "artist-01",
-    title: "SOLO",
-    year: 2016,
+    title: "Remember the Time",
+    year: 1991,
     duration: 260,
     flagged: false
   },
   {
     songID: "s6",
     artistID: "artist-01",
-    title: "V",
-    year: 2018,
+    title: "You Rock My World",
+    year: 2001,
     duration: 243,
     flagged: true
   },
   {
     songID: "s7",
     artistID: "artist-01",
-    title: "Drive",
-    year: 2016,
+    title: "Human Nature",
+    year: 1982,
     duration: 209,
     flagged: true
   },
   {
     songID: "s8",
     artistID: "artist-02",
-    title: "strawberry moon",
-    year: 2021,
+    title: "Superstition",
+    year: 1972,
     duration: 219,
     flagged: true
   },
   {
     songID: "s9",
     artistID: "artist-02",
-    title: "Lilac",
-    year: 2021,
+    title: "Master Blaster",
+    year: 1980,
     duration: 279,
     flagged: true
   },
   {
     songID: "s10",
     artistID: "artist-02",
-    title: "Celebrity",
-    year: 2021,
+    title: "For Once in My Life",
+    year: 1968,
     duration: 198,
     flagged: true
   },
   {
     songID: "s11",
     artistID: "artist-02",
-    title: "Eight",
-    year: 2020,
+    title: "Overjoyed",
+    year: 1985,
     duration: 199,
     flagged: true
   },
   {
     songID: "s12",
     artistID: "artist-02",
-    title: "Love poem",
-    year: 2019,
+    title: "Higher Ground",
+    year: 1973,
     duration: 215,
     flagged: true
   },
   {
     songID: "s13",
     artistID: "artist-02",
-    title: "Blueming",
-    year: 2019,
+    title: "My Cherie Amour",
+    year: 1969,
     duration: 221,
     flagged: false
   },
   {
     songID: "s14",
     artistID: "artist-02",
-    title: "Pipi",
-    year: 2018,
+    title: "All I Do",
+    year: 1980,
     duration: 208,
     flagged: true
   },
   {
     songID: "s15",
     artistID: "artist-03",
-    title: "I will go to you like the first snow",
-    year: 2017,
+    title: "Just to See Her",
+    year: 1987,
     duration: 230,
     flagged: true
   },
   {
     songID: "s16",
     artistID: "artist-03",
-    title: "Heaven",
-    year: 2012,
+    title: "I Second That Emotion",
+    year: 1968,
     duration: 223,
     flagged: true
   },
   {
     songID: "s17",
     artistID: "artist-03",
-    title: "If you",
-    year: 2016,
+    title: "Let Me Be the Clock",
+    year: 1980,
     duration: 221,
     flagged: true
   },
   {
     songID: "s18",
     artistID: "artist-03",
-    title: "I will show you",
-    year: 2012,
+    title: "One Heartbeat",
+    year: 1987,
     duration: 232,
     flagged: true
   },
   {
     songID: "s19",
     artistID: "artist-03",
-    title: "Evening sky",
-    year: 2012,
+    title: "Much Better Off",
+    year: 1968,
     duration: 228,
     flagged: true
   },
   {
     songID: "s20",
     artistID: "artist-03",
-    title: "U&I",
-    year: 2013,
+    title: "Love Machine",
+    year: 1975,
     duration: 217,
     flagged: false
   },
   {
     songID: "s21",
     artistID: "artist-03",
-    title: "Ice Flower",
-    year: 2013,
+    title: "Quiet Storm",
+    year: 1975,
     duration: 214,
     flagged: true
   },
